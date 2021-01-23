48188543a1cef69e7ab1b28bcdf6396e96d2baa5
Lidia Ataupillco <ataupill@ualberta.ca>
2021-01-23 16:55:41 -0700

    home page

--- main_app/loco4local/settings.py
+++ main_app/loco4local/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'home.apps.HomeConfig',
+    'home',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -141,3 +141,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
