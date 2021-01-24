0be7716aca71761ade8286f18ca76fb67eed10a6
Lidia Ataupillco <ataupill@ualberta.ca>
2021-01-23 18:51:17 -0700

    added create_business endpoint

--- main_app/home/urls.py
+++ main_app/home/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # http://localhost:8000/home/create_business/
+    path('create_business/', views.create_business, name='create-business')
 ]
--- main_app/home/views.py
+++ main_app/home/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'home.html', {})
+
+def create_business(request):
+    return render(request, 'create_business.html', {})
